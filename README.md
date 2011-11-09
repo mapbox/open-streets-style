4030beaa1ce1bd69f1e5a4cf05c9ba82ac834a1b
Dane Springmeyer <dane@dbsgeo.com>
2011-11-08 17:17:46 -0800

    add to readme, push safer defaults (for performance) into the config, and check in modified mml (with sorted keys)

--- README.md
+++ README.md
@@ -9,14 +9,70 @@ This style has been designed to work with a PostGIS database created with
 
 [1]: http://wiki.openstreetmap.org/wiki/Osm2pgsql
 
+## Requirements
+
+Python >= 2.6 (for setting up styles)
+
+TileMill >= 0.5.x
+
+Postgres/PostGIS installation
+
+Shapefile setup
+
+OSM Extract imported into postgres
+
+
 ## Setup (for TileMill)
 
-1. Edit the file `configure.py` so the database settings match your setup.
-2. From the `open-streets-style` directory, run `./configure.py`.
-3. Copy or link the `open-streets` subdirectory to your TileMill projects
-   folder (`<tilemill directory>/files/project/`).
+Now download and the styles:
+
+    git clone git://github.com/mapbox/open-streets-style.git
+    cd open-streets-style
+
+Download the osm shapefiles into the layers sub-directory:
+
+    cd open-streets
+    mkdir layers
+    cd layers
+    wget http://tilemill-data.s3.amazonaws.com/osm/processed_p.zip
+    unzip processed_p.zip
+    wget http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip
+    unzip shoreline_300.zip
+    cd ../../
+
+If you do not manually download these and provide the path during setup
+then they will be downloaded on the fly which will take a long time.
+
+Now edit the `configure.py` so the database and shapefiles settings match your setup.
+
+Then run the script:
+
+    python configure.py
+
+Now copy or link the projection subdirectory called 'open-streets' into
+your TileMill projects directory at:
+
+    ~/Documents/MapBox/project/
+
+You can make a symlink like:
+
+    ln -s `pwd`/open-streets ~/Documents/MapBox/project/open-streets
+
+Now go start TileMill and you should see the project available.
 
 Unless you specified paths to local copies of `processed_p.shp` and 
 `shoreline_300.shp` in config.py, these files will be downloaded the first
 time you load the project in TileMill. They total approximately **360 MB**,
 thus will take at least a few minutes to download.
+
+
+## Troubleshooting
+
+If your map appears blank then you likely have some error preventing the
+stylesheet from loading for the first time. Since you loaded the stylesheet
+through a backdoor symlink TileMill cannot protect you from manual setup 
+errors.
+
+Try viewing an individual tile to get access to an error:
+
+    http://localhost:8889/1.0.0/open-streets/0/0/0.png
--- configure.py
+++ configure.py
@@ -3,26 +3,37 @@
 ## OPEN STREETS CONFIGURATION OPTIONS ##
 
 # PostGIS connection setup
-host     = "localhost"
-port     = "5432"
-dbname   = "gis"
-user     = "gis"
-password = "gis"
+host     = "" # localhost
+port     = "" # 5432
+dbname   = "osm"
+user     = "" # postgres
+password = ""
 
 # Land shapefiles required for the style. If you have already downloaded
 # these or wish to use different versions, specify their paths here.
 # The latest versions can be downloaded from osm.org:
 # - http://tile.openstreetmap.org/processed_p.tar.bz2
 # - http://tile.openstreetmap.org/shoreline_300.tar.bz2
-processed_p = "http://tilemill-data.s3.amazonaws.com/osm/processed_p.zip"
-shoreline_300 = "http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip"
 
+## uncheck these lines to trigger automaticate download
+#processed_p = "http://tilemill-data.s3.amazonaws.com/osm/processed_p.zip"
+#shoreline_300 = "http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip"
+
+## these lines will look for these shapefiles locally,
+## assuming you downloaded them manually into the open-streets/layer directory
+processed_p = "./layers/processed_p.shp"
+shoreline_300 = "./layers/shoreline_300.shp"
+
+# For postgres layers mapnik by default will query postgis for the
+# extent to know whether to process the layer during rendering
 # Increase performance if you are only rendering a particular area by
-# specifying a bounding box to restrict queries. Format is "XMIN,YMIN,XMAX,YMAX" in the
+# specifying a bounding box in the format of "XMIN,YMIN,XMAX,YMAX" in the
 # same units as the database (probably spherical mercator meters). The
 # whole world is "-20037508.34,-20037508.34,20037508.34,20037508.34".
-# Leave blank to let Mapnik estimate.
-extent = ""
+extent = "-20037508.34,-20037508.34,20037508.34,20037508.34"
+
+# if you have > 2GB mem, turn this on for slightly faster rendering
+feat_caching = True
 
 #################################
 
@@ -38,15 +49,23 @@ f.closed
 
 with open(mml, 'w') as f:
   for layer in newf["Layer"]:
-    layer["Datasource"]["host"] = host
-    layer["Datasource"]["port"] = port
-    layer["Datasource"]["dbname"] = dbname
-    layer["Datasource"]["user"] = user
-    layer["Datasource"]["password"] = password
-    layer["Datasource"]["extent"] = extent
-    if layer["id"] == "shoreline_300":
-      layer["Datasource"]["file"] = shoreline_300
-    elif (layer["id"] == "processed_p") or (layer["id"] == "processed_p_outline"):
-      layer["Datasource"]["file"] = processed_p
-  f.write(json.dumps(newf, indent=2))
+    layer["properties"] = {}
+    if feat_caching:
+        layer["properties"]["cache-features"] = "true"
+    ds_type = layer["Datasource"].get("type")
+    if ds_type and ds_type == "postgis":
+        layer["Datasource"]["host"] = host
+        layer["Datasource"]["port"] = port
+        layer["Datasource"]["dbname"] = dbname
+        layer["Datasource"]["user"] = user
+        layer["Datasource"]["password"] = password
+        layer["Datasource"]["extent"] = extent
+        layer["Datasource"]["srid"] = 900913
+    ds_file = layer["Datasource"].get("file")
+    if ds_file:
+        if layer["id"] == "shoreline_300":
+          layer["Datasource"]["file"] = shoreline_300
+        elif (layer["id"] == "processed_p") or (layer["id"] == "processed_p_outline"):
+          layer["Datasource"]["file"] = processed_p
+  f.write(json.dumps(newf, sort_keys=True, indent=2))
 f.closed
